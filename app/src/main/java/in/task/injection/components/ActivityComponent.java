b944c3e6f96800ca83984ca609478ddb83a03700
VivekNeel <vivek2neel@gmail.com>
2017-07-21 19:17:53 +0530

    Func : Allow injection into fragments

--- app/src/main/java/in/task/injection/components/ActivityComponent.java
+++ app/src/main/java/in/task/injection/components/ActivityComponent.java
@@ -5,6 +5,9 @@ import javax.inject.Singleton;
 import dagger.Component;
 import in.task.injection.PerActivity;
 import in.task.injection.module.ActivityModule;
+import in.task.ui.forgotpassword.ForgotPasswordFragment;
+import in.task.ui.login.LoginFragment;
+import in.task.ui.register.RegisterFragment;
 
 /**
  * Created by vivek on 21/07/17.
@@ -14,5 +17,9 @@ import in.task.injection.module.ActivityModule;
 @Component(dependencies = ApplicationComponent.class, modules = ActivityModule.class)
 public interface ActivityComponent {
 
+    void inject(RegisterFragment registerFragment);
 
+    void inject(LoginFragment loginFragment);
+
+    void inject(ForgotPasswordFragment forgotPasswordFragment);
 }
--- app/src/main/java/in/task/injection/components/ApplicationComponent.java
+++ app/src/main/java/in/task/injection/components/ApplicationComponent.java
@@ -1,10 +1,15 @@
 package in.task.injection.components;
 
+import android.app.Application;
+import android.content.Context;
+
 import javax.inject.Singleton;
 
 import dagger.Component;
 import dagger.Module;
 import in.task.ZoloApplication;
+import in.task.data.DataManagerLogic;
+import in.task.injection.ApplicationContext;
 import in.task.injection.module.ApplicationModule;
 
 /**
@@ -15,5 +20,13 @@ import in.task.injection.module.ApplicationModule;
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
 
-    void injectInto(ZoloApplication zoloApplication);
+    @ApplicationContext
+    Context context();
+
+    void inject(ZoloApplication zoloApplication);
+
+    DataManagerLogic datamangerLogic();
+
+    Application application();
+
 }
